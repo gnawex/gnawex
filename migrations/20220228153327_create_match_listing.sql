de40ca6b2c98469bfed54ee9a7b1005ae60039fb
SEKUN <sekun@hey.com>
2022-03-03 20:02:24 +0800

    Refactor transaction creation
    This now creates multiple transactions to accommodate the new changes
    in matching a listing against multiple listings.

--- migrations/20220228153327_create_match_listing.sql
+++ migrations/20220228153327_create_match_listing.sql
@@ -9,10 +9,13 @@ GRANT anon TO gnawex_merchant;
 --------------------------------------------------------------------------------
 -- Procedures
 
--- TODO: Add creation of transaction(s)
-CREATE OR REPLACE PROCEDURE transact(matchee_id BIGINT, matchee_item_id BIGINT,
-  matchee_user_id BIGINT, matchee_quantity INTEGER, matchee_type LISTING_TYPE,
-  matchee_cost INTEGER) AS $$
+--------------------------------------------------------------------------------
+-- Functions
+
+-- NOTE: `trigger_match/0` is in the same transaction as the one that called it.
+-- So if ever something gets inserted, then any of this fails, it will also
+-- rollback the original insert operation.
+CREATE OR REPLACE FUNCTION match() RETURNS TRIGGER AS $$
 BEGIN
   -- Need `gnawex_merchant` to update the involved listings because `valid_user`
   -- can't. Users are not permitted to modify the listing once created.
@@ -20,6 +23,9 @@ BEGIN
   -- altered after a transaction exists. I suppose altering `is_active` is
   -- fine.
   SET LOCAL ROLE gnawex_merchant;
+
+  -- Brace yourself for loads of CTEs. I don't know if I want it like this but
+  -- hey, if it ain't broke don't fix it.
 
   -- Ok I know this word doesn't exist but I don't know what else to call it.
   WITH matches_cte AS (
@@ -30,52 +36,86 @@ BEGIN
     -- 3. Listings not created by the matchee's user
     -- 4. Same `cost`
     -- 5. If it's active (`is_active`)
-    SELECT id, quantity, item_id, sum(quantity) OVER (ORDER BY id ASC) AS running_amount
+    SELECT id, quantity, item_id, user_id, sum(quantity) OVER (ORDER BY id ASC) AS running_amount
       FROM listings
-      WHERE item_id = matchee_item_id
+      WHERE item_id = NEW.item_id
         AND is_active = TRUE
         AND type = (
-          CASE WHEN matchee_type = 'buy' THEN 'sell'
+          CASE WHEN NEW.type = 'buy' THEN 'sell'
                ELSE 'buy'
           END
         ) :: LISTING_TYPE
-        AND user_id != matchee_user_id
-        AND cost = matchee_cost
+        AND user_id != NEW.user_id
+        AND cost = NEW.cost
   ), total_cte AS (
     -- Further filters the matches to fulfill the matchee's quantity.
-    SELECT id, quantity, running_amount, sum(quantity) OVER (partition BY item_id) AS total_quantity
+    SELECT id, quantity, user_id, running_amount, sum(quantity) OVER (partition BY item_id) AS total_quantity
       FROM matches_cte
-      WHERE running_amount - quantity <= matchee_quantity
-  ), update_cte AS (
+      WHERE running_amount - quantity <= NEW.quantity
+  ), update_matchee AS (
+    -- Updates the matchee with the new quantity after it got matched with
+    -- other listing(s).
     UPDATE listings
-      SET quantity = (
-        CASE
-          WHEN
-            total_cte.running_amount - total_cte.total_quantity = 0 AND
-              total_cte.total_quantity > matchee_quantity
-          THEN total_cte.total_quantity - matchee_quantity
-
-          ELSE
-            GREATEST(total_cte.running_amount - total_cte.total_quantity, 0) :: INT
-
-        END
-      )
+      SET quantity = GREATEST(total_cte.running_amount - total_cte.total_quantity, 0) :: INT
       FROM total_cte
-      WHERE listings.id = total_cte.id
+      WHERE listings.id = NEW.id
+  ), update_matches AS (
+   -- Updates the matches after being matched with the matchee.
+    UPDATE listings
+        SET quantity = (
+          CASE
+            -- `total_cte.total_quantity` doesn't reflect the matchee's
+            -- quantity! In fact, it only represents the total quantity fetched
+            -- that may (at least) fulfill the matchee. The `total_quantity`
+            -- can exceed `NEW.quantity` (the matchee quantity)!
+            -- 
+            -- So, you have to check two things:
+            --
+            -- (1)
+            -- `total_cte.running_amount - total_cte.total_quantity = 0`
+            -- Because you need to make sure you're at the last matched listing.
+            -- It _always_ zeroes out at the end of the listing.
+            --
+            -- (2)
+            -- `total_cte.total_quantity > NEW.quantity`
+            -- Since you have to check if the last matching listing should have
+            -- a new quantity of 0. If the `total_quantity` does exceed the
+            -- actual quantity, then you have to subtract it. 
+            -- e.g total_quantity = 5, quantity = 3. The last match's quantity
+            -- is going to be 2.
+            --
+            -- When either of these conditions fail, the idea is to zero out
+            -- the match's quantity.
+            WHEN
+              total_cte.running_amount - total_cte.total_quantity = 0 AND
+                total_cte.total_quantity > NEW.quantity
+            THEN total_cte.total_quantity - NEW.quantity
+
+            ELSE
+              GREATEST(total_cte.running_amount - total_cte.total_quantity, 0) :: INT
+
+          END
+        )
+        FROM total_cte
+        WHERE listings.id = total_cte.id
   )
-  UPDATE listings
-    SET quantity = GREATEST(total_cte.running_amount - total_cte.total_quantity, 0) :: INT
-    FROM total_cte
-    WHERE listings.id = matchee_id;
-END;
-$$ LANGUAGE plpgsql;
+  INSERT INTO transactions (buy_order, sell_order, buyer_id, seller_id)
+    (
+      SELECT
+        CASE WHEN NEW.type = 'buy' THEN NEW.id ELSE total_cte.id
+        END AS buy_order,
 
---------------------------------------------------------------------------------
--- Functions
+        CASE WHEN NEW.type = 'buy' THEN total_cte.id ELSE NEW.id
+        END AS sell_order,
 
-CREATE OR REPLACE FUNCTION match() RETURNS TRIGGER AS $$
-BEGIN
-  CALL transact(NEW.id, NEW.item_id, NEW.user_id, NEW.quantity, NEW.type, NEW.cost);
+        CASE WHEN NEW.type = 'buy' THEN NEW.user_id ELSE total_cte.user_id
+        END AS buyer_id,
+
+        CASE WHEN NEW.type = 'buy' THEN total_cte.user_id ELSE NEW.user_id
+        END AS seller_id
+
+        FROM total_cte
+    );
 
   RETURN NULL;
 END;
@@ -91,3 +131,4 @@ CREATE OR REPLACE TRIGGER match_listings
     EXECUTE PROCEDURE match();
 
 --------------------------------------------------------------------------------
+
