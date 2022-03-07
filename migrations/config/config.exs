0884b6fed5d2c26458a595d4b2425984feac9289
SEKUN <sekun@hey.com>
2022-03-07 19:47:58 +0800

    chore: Add `sqitch`

--- migrations/config/config.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :migrations, ecto_repos: [Migrations.Repo]
-
-config :migrations, Migrations.Repo,
-  database: "gnawex_db",
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost"
