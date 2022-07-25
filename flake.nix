585a2e78e85fccc3b916b3bbae812154700301da
SEKUN <sekun@hey.com>
2022-07-26 01:02:55 +0800

    refactor: Remove PostgREST
    It's good, but there are some deal-breakers I don't want to run into so
    I'm more keen in writing my own server.

--- flake.nix
+++ flake.nix
@@ -4,28 +4,20 @@
   inputs = {
      nixpkgs.url = "github:NixOS/nixpkgs";
      masterpkgs.url = "github:NixOS/nixpkgs/master";
-     postgrestPkg.url = "path:./nix/postgrest";
      flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, masterpkgs, postgrestPkg, flake-utils }:
+  outputs = { self, nixpkgs, masterpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-          postgrest = postgrestPkg.defaultPackage.${system};
           lib =  nixpkgs.lib;
       in {
         devShell = pkgs.mkShell rec {
           buildInputs = [
             pkgs.sqitchPg
-            postgrest
-            masterpkgs.legacyPackages.${system}.pgadmin4
             pkgs.insomnia
             pkgs.perl534Packages.TAPParserSourceHandlerpgTAP
           ];
-
-          shellHook = ''
-          source bin/seed
-          '';
         };
       });
 }
