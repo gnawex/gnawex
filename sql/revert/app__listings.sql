179c7da1a5aa5f3596c4572fa9e6f287dbdb0919
SEKUN <sekun@hey.com>
2022-12-27 13:05:47 +0800

    fix: Drop index properly

--- sql/revert/app__listings.sql
+++ sql/revert/app__listings.sql
@@ -16,7 +16,7 @@ REVOKE SELECT, UPDATE ON TABLE app.listings FROM gnawex_merchant;
 
 -- FIXME: This is broken for some reason. Why does dropping an index not work?
 -- It says it doesn't exist. Hmm...
-DROP INDEX IF EXISTS active_id;
+DROP INDEX IF EXISTS app.active_id;
 
 DROP TABLE app.listings;
 DROP TYPE app.LISTING_TYPE;
