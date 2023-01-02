9d13d2719faa4b50a2ff5fe21746113c30a653c6
SEKUN <sekun@hey.com>
2023-01-02 23:24:48 +0800

    feat: Add `current_unit_quantity` to listing

--- sql/verify/app__listings.sql
+++ sql/verify/app__listings.sql
@@ -3,16 +3,17 @@
 BEGIN;
 
 SELECT
-    id,
-    tradable_item__id,
-    user__id,
-    unit_quantity,
-    cost,
-    type,
-    batched_by,
-    active,
-    created_at,
-    updated_at
+    id
+  , tradable_item__id
+  , user__id
+  , unit_quantity
+  , current_unit_quantity
+  , cost
+  , type
+  , batched_by
+  , active
+  , created_at
+  , updated_at
   FROM app.tradable_item_listings
   WHERE false;
 
