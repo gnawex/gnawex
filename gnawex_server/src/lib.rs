b8f1b7af79f0136f83a495588afc3f416ac0bae0
SEKUN <software@sekun.net>
2023-07-16 08:52:36 +0000

    feat: Item order creation (#73)

--- gnawex_server/src/lib.rs
+++ gnawex_server/src/lib.rs
@@ -1,9 +1,14 @@
 use axum::{
     extract::{Path, State},
-    routing::get,
-    Router, Server,
+    routing::{get, post},
+    Form, Router, Server,
+};
+use gnawex_core::{
+    item,
+    item_order::{self, Buy, CreateListing, ItemOrder, Sell},
 };
 use gnawex_html::app::{ItemIndexPage, ItemShowPage};
+use serde::Deserialize;
 use std::{net::SocketAddr, sync::Arc};
 use tower_http::services::ServeDir;
 
@@ -39,10 +44,82 @@ fn app() -> Router {
     let app_state = Arc::new(AppState { db_handle });
 
     Router::new()
+        // HTML content type routes
         .route("/items", get(item_index))
         .route("/items/:id", get(item_show))
+        .route("/items/:id", post(item_order_create))
         .nest_service("/assets", ServeDir::new("assets"))
         .with_state(app_state)
+}
+
+#[derive(Debug, Deserialize)]
+struct NewItemOrder {
+    batched_by: i16,
+    cost: i32,
+    unit_quantity: i32,
+    kind: item_order::Type,
+}
+
+async fn item_order_create(
+    State(state): State<Arc<AppState>>,
+    Path(item_id): Path<item::Id>,
+    Form(new_order): Form<NewItemOrder>,
+) -> ItemShowPage {
+    println!("{:#?}", item_id);
+    println!("{:#?}", new_order);
+
+    // TODO: Refactor to have these in the same txn (create & match, get grouped orders)
+    // TODO: Refactor to extract user ID from session instead of it being hardcoded
+    // TODO: Remove unwraps
+    // TODO: Render error page when any of the operations fail.
+
+    match new_order.kind {
+        item_order::Type::Buy => {
+            let buy = Buy::create_and_match(
+                &state.db_handle,
+                CreateListing {
+                    item_id,
+                    user_id: 2,
+                    batched_by: new_order.batched_by,
+                    unit_quantity: new_order.unit_quantity,
+                    cost: new_order.cost,
+                },
+            )
+            .await;
+
+            println!("{:#?}", buy);
+        }
+        item_order::Type::Sell => {
+            let sell = Sell::create_and_match(
+                &state.db_handle,
+                CreateListing {
+                    item_id,
+                    user_id: 2,
+                    batched_by: new_order.batched_by,
+                    unit_quantity: new_order.unit_quantity,
+                    cost: new_order.cost,
+                },
+            )
+            .await;
+
+            println!("{:#?}", sell);
+        }
+    };
+
+    let item = gnawex_core::item::get_item(&state.db_handle, item_id)
+        .await
+        .unwrap();
+
+    let grouped_orders =
+        gnawex_core::item_grouped_order::get_grouped_orders_by_item_id(&state.db_handle, item_id)
+            .await
+            .unwrap();
+
+    gnawex_html::app::ItemShowPage {
+        item,
+        grouped_buy_orders: grouped_orders.buy_orders,
+        grouped_sell_orders: grouped_orders.sell_orders,
+    }
 }
 
 async fn item_index(State(state): State<Arc<AppState>>) -> ItemIndexPage {
@@ -62,6 +139,7 @@ async fn item_index(State(state): State<Arc<AppState>>) -> ItemIndexPage {
 }
 
 async fn item_show(Path(id): Path<i64>, State(state): State<Arc<AppState>>) -> ItemShowPage {
+    // TODO: Handle errors by rendering error page
     let item = gnawex_core::item::get_item(&state.db_handle, gnawex_core::item::Id(id))
         .await
         .unwrap();
