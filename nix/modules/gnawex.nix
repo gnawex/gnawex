f115af6fc346760035edbe043ecef35ffc4bdc54
SEKUN <software@sekun.net>
2023-08-19 22:39:42 +0800

    chore: Rename env

--- nix/modules/gnawex.nix
+++ nix/modules/gnawex.nix
@@ -31,17 +31,17 @@ with lib; {
 
       environment = mkMerge [
         {
-          GX_PORT = "${cfg.port}";
-          GX_DB_NAME = "${cfg.db_name}";
-          GX_DB_HOST = "${cfg.db_host}";
-          GX_DB_PORT = "${cfg.db_port}";
-          GX_DB_USER = "${cfg.db_user}";
-          GX_DB_PASSWORD_FILE = "${cfg.db_password_file}";
-          GX_DB_POOL_SIZE = "${cfg.db_pool_size}";
+          GX__PORT = "${cfg.port}";
+          GX__DB_NAME = "${cfg.db_name}";
+          GX__DB_HOST = "${cfg.db_host}";
+          GX__DB_PORT = "${cfg.db_port}";
+          GX__DB_USER = "${cfg.db_user}";
+          GX__DB_PASSWORD_FILE = "${cfg.db_password_file}";
+          GX__DB_POOL_SIZE = "${cfg.db_pool_size}";
         }
 
         (mkIf ("${cfg.db_ca_cert_file}" != "") {
-          GX_DB_CA_CERT_FILE = "${cfg.db_ca_cert_file}";
+          GX__DB_CA_CERT_FILE = "${cfg.db_ca_cert_file}";
         })
       ];
     };
